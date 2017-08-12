c35fd1be2c6aa8d464a588f088717aebe4078839
Potemkyne <potemkyne.github@gmail.com>
2017-08-13 00:07:05 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+
+  export class AppComponent {
+    movie: FirebaseListObservable <any>;
+
+    constructor(db: AngularFireDatabase) {
+      this.movie = db.list('/movies');
+    }
+
+    updateMovie(key: string, updatetitle: string, updatedate: string , updateplace: string, updateprojectionnist: string){
+      this.movie.update(key, { title: updatetitle , date: updatedate, projectionnist: updateprojectionnist});
+    }
+
+    deleteMovie(key: string){
+      this.movie.remove(key);
+    }
+
+    addMovie(newTitle: string , newDate: string, newPlace: string, newProjectionnist: string ) {
+      this.movie.push({ title: newTitle , date: newDate, place: newPlace, projectionnist: newProjectionnist} );
+    }
+
+    deleteEverything() {
+      this.movie.remove();
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "XXXXXX",
+    authDomain: "XXXXXX.firebaseapp.com",
+    databaseURL: "https://XXXXXX.firebaseio.com",
+    projectId: "XXXXXX",
+    storageBucket: "XXXXXX.appspot.com",
+    messagingSenderId: "XXXXXX"
+  }
 };
